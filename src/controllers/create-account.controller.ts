7f5f0b04edf6afdf205bcd3f10348d413135a99f
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-11-20 23:16:49 -0300

    :sparkles: feat : validation pipe with zod

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,17 +4,29 @@ import {
   Controller,
   HttpCode,
   Post,
+  UsePipes,
 } from '@nestjs/common'
 import { hash } from 'bcrypt'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchma = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
+@UsePipes(new ZodValidationPipe(createAccountBodySchema))
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  async handle(@Body() body: CreateAccountBodySchma) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
