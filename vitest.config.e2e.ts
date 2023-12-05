6de31d94004de1d77d1996a16441b7f42ae0cbca
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-12-04 22:47:15 -0300

    :test_tube: config: vitest config

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,17 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'], // pegar apenas testes com a extensão e2e
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
