49473c4a0469190f2603b519457c203985dad69d
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-12-10 11:03:10 -0300

    :sparkles: feat: prisma to domain mapper

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService], // todo módulo que importar o DatabaseModule também vai ter acesso ao PrismaService
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ], // todo módulo que importar o DatabaseModule também vai ter acesso ao PrismaService
 })
 export class DatabaseModule {}
---
+++ src/infra/database/prisma/mappers/prisma-quesition-mapper.ts
@@ -0,0 +1,29 @@
+// pegar uma question do prisma e converter para o formato do dominio
+// é a mesma question, porem cm formato diferente
+// static pois não iremos instanciar a classe
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
+import { Question as PrismaQuestion } from '@prisma/client'
+
+export class PrismaQuestionMapper {
+  static toDoamin(raw: PrismaQuestion): Question {
+    return Question.create(
+      {
+        title: raw.title,
+        content: raw.content,
+        authorId: new UniqueEntityId(raw.authorId),
+        bestAnswerId: undefined,
+        slug: Slug.create(raw.slug),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+}
+
+// undefined vs null
+// undeined: valor nao existe
+// null: valor não preenchido
