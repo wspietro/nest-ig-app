744b5ec77ef19982126f4c01720f535f70d10287
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-12-26 16:58:46 -0300

    :recycle: refacto: fetch recent questions

--- src/domain/forum/application/use-cases/fetch-recent-questions.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -1,6 +1,7 @@
 import { Either, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
+import { Injectable } from '@nestjs/common'
 
 interface FetchRecentQuestionsRequest {
   page: number // paginação
@@ -8,6 +9,7 @@ interface FetchRecentQuestionsRequest {
 
 type FetchRecentQuestionsResponse = Either<null, { questions: Question[] }>
 
+@Injectable()
 export class FetchRecentQuestionsUseCase {
   constructor(private questionRepository: QuestionsRepository) {}
 
--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 
 const pageQueryParamSchema = z
   .string()
@@ -18,19 +18,11 @@ type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 @Controller('/questions')
 @UseGuards(JwtAuthGuard) // usar estratégia passport-jwt
 export class FetchRecentQuestionsController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const recordsPerPage = 20
-
-    const questions = await this.prisma.question.findMany({
-      take: recordsPerPage, // select limited range per page
-      skip: (page - 1) * recordsPerPage, // records i want to skip (pages). Pula registros necessários para mostrar os da página solicitada
-      orderBy: {
-        createdAt: 'desc',
-      },
-    })
+    const questions = await this.fetchRecentQuestions.execute({ page })
 
     return { questions }
   }
