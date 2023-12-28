bc1cf569c17445a5e510c22dcd85a16bb15623c7
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-12-27 21:55:09 -0300

    :construction: feat: crypto gateways contracts

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,9 +3,23 @@ import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.name
+  }
+
+  get passowrd() {
+    return this.props.name
+  }
+
   static create(props: StudentProps, id?: UniqueEntityId) {
     const student = new Student(props, id)
 
