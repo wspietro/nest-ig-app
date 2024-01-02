42645a2e09df76ea2f405689cc68e999f1dfde93
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-01-02 09:36:59 -0300

    :sparkles: feat: crypto gateways implementation

---
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,17 @@
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+import { Injectable } from '@nestjs/common'
+import { compare, hash } from 'bcrypt'
+
+@Injectable()
+export class BcryptHasher implements HashComparer, HashGenerator {
+  private HASH_SALT_LENGHT = 8
+
+  hash(plain: string): Promise<string> {
+    return hash(plain, this.HASH_SALT_LENGHT)
+  }
+
+  compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash)
+  }
+}
