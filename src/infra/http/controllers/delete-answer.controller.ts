2db3c53179da69af0d6b32d4a1c0f00f2fe1aef3
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-01-07 14:26:43 -0300

    :sparkles: feat: delete-answer controller

---
+++ src/infra/http/controllers/delete-answer.controller.ts
@@ -0,0 +1,35 @@
+import {
+  BadRequestException,
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
+
+@Controller('/answers/:id')
+export class DeleteAnswerController {
+  constructor(private deleteAnswer: DeleteAnswerUseCase) {}
+
+  @Delete()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('id') answerId: string,
+  ) {
+    const userId = user.sub
+
+    const result = await this.deleteAnswer.execute({
+      authorId: userId,
+      answerId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+    // não fazemos a tratativa como em authenticate pois não é tão comum cair nesse erro;
+    // fazemos tratativa dos erros esperados.
+  }
+}
