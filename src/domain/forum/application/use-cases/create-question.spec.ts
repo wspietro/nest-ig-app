52c998fc94ba4a57863109ed138a930b790c6b56
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-01-23 22:39:51 -0300

    :sparkles: feat: edit and create question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -38,4 +38,26 @@ describe('Create Question', () => {
       expect.objectContaining({ attachmentId: new UniqueEntityId('2') }),
     ])
   })
+
+  test('it should persist attachment when creating a new question', async () => {
+    const result = await sut.execute({
+      authorId: '1',
+      title: 'Title Teste QuestionUseCase.',
+      content: 'Content Teste QuestionUseCase.',
+      attachmentsIds: ['1', '2'],
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(2)
+    expect(inMemoryQuestionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityId('1'),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityId('2'),
+        }),
+      ]),
+    )
+  })
 })
