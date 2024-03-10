7c6af18230e4843271b9d1fb9c0fea2231014f3f
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2024-03-10 12:16:41 -0300

    :construction: feat: implementing redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common'
 import { EnvModule } from '../env/env.module'
+import { CacheRepository } from './cache-repository'
+import { ReddisCacheRepository } from './redis/reddis-cache-repository'
+import { RedisService } from './redis/redis.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvModule],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: ReddisCacheRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
